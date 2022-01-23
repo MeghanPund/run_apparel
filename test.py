d27d7f1edae7739a899aaa8b5dc986d18ea536f8
Meghan Pund <meghan.pund@gmail.com>
2022-01-23 12:21:57 -0500

    reformatted string to f string

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/temperature', methods=['POST'])
 def temperature():
     zipcode = request.form['zip']
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=fd38d62aa4fe1a03d86eee91fcd69f6e')
+    r = requests.get(f'http://api.openweathermap.org/data/2.5/weather?zip={zipcode},us&appid=fd38d62aa4fe1a03d86eee91fcd69f6e')
     json_object = r.json()
     temp_k = float(json_object['main']['temp'])
     temp_f = (temp_k - 273.15) * 1.8 + 32
